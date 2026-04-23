242d85d0c0581c0396da09a5c566549b95ab81db
jihoon <wlgnstls0413@naver.com>
2026-04-23 10:32:48 +0900

    first commit

---
+++ src/main/java/com/example/I_Commerce/ICommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.I_Commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/I_Commerce/ICommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.I_Commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
