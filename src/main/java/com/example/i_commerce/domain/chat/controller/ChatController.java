703c7c1f51ef30e8dace437910d12a45de4ad6e4
jeongkuk <herojk5019@naver.com>
2026-04-30 12:13:08 +0900

    fix: 컨트롤러 파라미터 어노테이션 변경

--- src/main/java/com/example/i_commerce/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/i_commerce/domain/chat/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.example.i_commerce.domain.chat.service.ChatService;
 import com.example.i_commerce.domain.order.service.OrderService;
 import com.example.i_commerce.global.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +18,7 @@ public class ChatController {
 //  개인 1:1 채팅방 개설
     @PostMapping("/room/create")
     public ApiResponse<Long> getOrCreatePrivateRoom(
-        @RequestParam Long myId, @RequestParam Long otherMemberId){
+        @PathVariable Long myId, @PathVariable Long otherMemberId){
 
         return chatService.getOrCreatePrivateRoom(myId, otherMemberId);
     }
