a7f002f5b19094b7e13473d49d0b2e64fae5e7aa
muncool39 <muncool39@gmail.com>
2026-05-06 21:15:26 +0900

    feat: 상품 상세 조회 쿼리 (#35)

---
+++ src/main/java/com/example/i_commerce/domain/product/repository/ProductQueryRepository.java
@@ -0,0 +1,38 @@
+package com.example.i_commerce.domain.product.repository;
+
+
+
+import com.example.i_commerce.domain.product.entity.Product;
+import com.example.i_commerce.domain.product.entity.QProduct;
+import com.example.i_commerce.domain.product.entity.QProductItem;
+import com.example.i_commerce.domain.product.entity.QProductOptionValue;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@RequiredArgsConstructor
+public class ProductQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    QProductOptionValue productOptionValue1 = new QProductOptionValue("ov1");
+    QProductOptionValue productOptionValue2 = new QProductOptionValue("ov2");
+
+    public Optional<Product> findProductWithItems(Long productId) {
+        Product result = queryFactory
+            .selectFrom(QProduct.product)
+            .distinct()
+            .leftJoin(QProduct.product.items, QProductItem.productItem).fetchJoin()
+            .leftJoin(QProductItem.productItem.optionValue1, productOptionValue1).fetchJoin()
+            .leftJoin(QProductItem.productItem.optionValue2, productOptionValue2).fetchJoin()
+            .leftJoin(QProduct.product.category).fetchJoin()
+            .where(QProduct.product.id.eq(productId))
+            .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+
+}
