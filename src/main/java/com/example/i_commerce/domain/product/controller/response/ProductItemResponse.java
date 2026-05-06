b0f1b83dacfa00c099f5ad4aa565a6d39293efa0
muncool39 <muncool39@gmail.com>
2026-05-06 20:45:43 +0900

    feat: 응답 객체 생성 (#35)

---
+++ src/main/java/com/example/i_commerce/domain/product/controller/response/ProductItemResponse.java
@@ -0,0 +1,37 @@
+package com.example.i_commerce.domain.product.controller.response;
+
+import com.example.i_commerce.domain.product.entity.ProductAttribute;
+import com.example.i_commerce.domain.product.entity.ProductItem;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Builder;
+
+@Builder
+public record ProductItemResponse(
+    Long itemId,
+    String sku,
+    Integer price,
+    String mainImageUrl,
+    String status,
+    String displayOptionName,
+    List<ProductAttributeResponse> attributes
+) {
+
+    public static ProductItemResponse of(
+        ProductItem item,
+        List<ProductAttribute> attributes
+    ) {
+        return ProductItemResponse.builder()
+            .itemId(item.getId())
+            .sku(item.getSku())
+            .price(item.getPrice())
+            .mainImageUrl(item.getMainImageUrl())
+            .status(item.getStatus().name())
+            .displayOptionName(item.getDisplayOptionName())
+            .attributes(attributes.stream()
+                .map(ProductAttributeResponse::of)
+                .collect(Collectors.toList())
+            )
+            .build();
+    }
+}
