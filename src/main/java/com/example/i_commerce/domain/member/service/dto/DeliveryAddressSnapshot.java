656ae942d47baa8f4660b5dab0da338b6c049027
zzimzzim <kju1354@naver.com>
2026-05-07 01:26:15 +0900

    주석추가

--- src/main/java/com/example/i_commerce/domain/member/service/dto/DeliveryAddressSnapshot.java
+++ src/main/java/com/example/i_commerce/domain/member/service/dto/DeliveryAddressSnapshot.java
@@ -1,14 +1,30 @@
 package com.example.i_commerce.domain.member.service.dto;
 
-public record DeliveryAddressSnapshot(//주문쪽으로 보낼 정보들
-                                      String recipientName,
-                                      String recipientPhone,
-                                      String zipCode,
-                                      String roadAddress,
-                                      String jibunAddress,
-                                      String detailAddress,
-                                      String extraAddress,
-                                      String deliveryMemo
+//주문쪽으로 보낼 정보들
+public record DeliveryAddressSnapshot(
+    //수령자 이름
+    String recipientName,
+
+    //수령자 전화번호
+    String recipientPhone,
+
+    //우편번호
+    String zipCode,
+
+    //도로면 주소
+    String roadAddress,
+
+    //지번주소
+    String jibunAddress,
+
+    //상세주소
+    String detailAddress,
+
+    //참고항목
+    String extraAddress,
+
+    //배송 메모
+    String deliveryMemo
 ) {
 
 }
