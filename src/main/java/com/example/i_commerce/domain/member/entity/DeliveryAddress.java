9b3ada0e49bec17d2952aed9bb55f31a7f2cf06d
jihoon <wlgnstls0413@naver.com>
2026-05-08 15:25:58 +0900

    feat: 회원 도메인 관련 에러 및 요청 정보

--- src/main/java/com/example/i_commerce/domain/member/entity/DeliveryAddress.java
+++ src/main/java/com/example/i_commerce/domain/member/entity/DeliveryAddress.java
@@ -3,10 +3,13 @@ package com.example.i_commerce.domain.member.entity;
 import com.example.i_commerce.global.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -26,9 +29,9 @@ public class DeliveryAddress extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "member_id", insertable = false, updatable = false)
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", insertable = false, updatable = false)
+    private Member member;
 
     /**
      * 배송지는 회원 기준으로 조회하므로 user_id는 평문 FK 유지
