d08251197dd127df04b62ee6e9ce0d167fe90efd
muncool39 <muncool39@gmail.com>
2026-05-10 23:48:55 +0900

    feat: 검색 제약사항 ErrorCode 추가 (#42)

--- src/main/java/com/example/i_commerce/domain/product/exception/ProductErrorCode.java
+++ src/main/java/com/example/i_commerce/domain/product/exception/ProductErrorCode.java
@@ -27,6 +27,10 @@ public enum ProductErrorCode implements ErrorCode {
     INVALID_OPTION(HttpStatus.CONFLICT, "PRD-40902", "요청한 옵션 값이 유효하지 않습니다."),
     EXCEEDED_MAX_OPTION(HttpStatus.BAD_REQUEST, "PRD-40903", "최대 옵션 수를 초과했습니다."),
 
+    SEARCH_KEYWORD_TOO_SHORT(HttpStatus.CONFLICT, "PRD-40904", "검색어는 2글자 이상이여야 합니다."),
+
+    GUEST_PAGE_LIMIT_EXCEEDED(HttpStatus.FORBIDDEN, "PRD-40301", "비로그인 사용자는 더 이상 페이지를 조회할 수 없습니다."),
+
     INVALID_OPTION_TYPE(HttpStatus.CONFLICT, "PRD-40905", "존재하지 않는 옵션 타입입니다."),
     INSUFFICIENT_STOCK(HttpStatus.CONFLICT, "PRD-40903", "재고가 충분하지 않습니다.")
 
