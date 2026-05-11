b3b79f784fcf9f674e6553395d0182fd3e02b974
muncool39 <muncool39@gmail.com>
2026-05-11 09:10:35 +0900

    chore: 검색어 길이 에러 코드 수정 (#42)

--- src/main/java/com/example/i_commerce/domain/product/exception/ProductErrorCode.java
+++ src/main/java/com/example/i_commerce/domain/product/exception/ProductErrorCode.java
@@ -27,7 +27,7 @@ public enum ProductErrorCode implements ErrorCode {
     INVALID_OPTION(HttpStatus.CONFLICT, "PRD-40902", "요청한 옵션 값이 유효하지 않습니다."),
     EXCEEDED_MAX_OPTION(HttpStatus.BAD_REQUEST, "PRD-40903", "최대 옵션 수를 초과했습니다."),
 
-    SEARCH_KEYWORD_TOO_SHORT(HttpStatus.CONFLICT, "PRD-40904", "검색어는 2글자 이상이여야 합니다."),
+    SEARCH_KEYWORD_TOO_SHORT(HttpStatus.BAD_REQUEST, "PRD-40001", "검색어는 2글자 이상이어야 합니다."),
 
     GUEST_PAGE_LIMIT_EXCEEDED(HttpStatus.FORBIDDEN, "PRD-40301", "비로그인 사용자는 더 이상 페이지를 조회할 수 없습니다."),
 
