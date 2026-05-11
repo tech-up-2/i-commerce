52cd4803e38c82dbf3cdd7568ffc64bcd172c5fd
muncool39 <muncool39@gmail.com>
2026-05-11 16:42:38 +0900

    feat: 카테고리 전체 조회 컨트롤러 (#50)

---
+++ src/main/java/com/example/i_commerce/domain/product/controller/CategoryController.java
@@ -0,0 +1,29 @@
+package com.example.i_commerce.domain.product.controller;
+
+
+import com.example.i_commerce.domain.product.application.service.CategoryService;
+import com.example.i_commerce.domain.product.controller.response.CategoryResponse;
+import com.example.i_commerce.global.common.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ApiResponse<List<CategoryResponse>> getAllCategories(
+        @RequestParam(required = false) Integer maxDepth
+    ) {
+        return ApiResponse.success(categoryService.getAllCategories(maxDepth));
+    }
+
+}
