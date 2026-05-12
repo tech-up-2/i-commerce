d33c98d8778c8494b49bb17721523ec69a2c46dd
jihoon <wlgnstls0413@naver.com>
2026-05-12 17:47:35 +0900

    feat: 결제 취소 요청시 배송 상태 변경 이벤트 처리
    - 결제를 취소하기 전 먼저 배송 상태를 CANCELED로 만들어 놓기 위해 이벤트 처리
    - 추후 포인트, 부분 취소 등의 기능을 위해 느슨한 결합을 위해 동기식 이벤트 처리 사용

--- src/main/java/com/example/i_commerce/domain/order/event/listener/DeliveryListener.java
+++ src/main/java/com/example/i_commerce/domain/order/event/listener/DeliveryListener.java
@@ -3,10 +3,14 @@ package com.example.i_commerce.domain.order.event.listener;
 import com.example.i_commerce.domain.order.entity.Delivery;
 import com.example.i_commerce.domain.order.entity.emuns.DeliveryStatus;
 import com.example.i_commerce.domain.order.event.dto.PaymentCompletedEvent;
+<<<<<<<< HEAD:src/main/java/com/example/i_commerce/domain/order/event/listener/PaymentLogListener.java
+import com.example.i_commerce.domain.order.service.PaymentHistoryService;
+========
 import com.example.i_commerce.domain.order.service.DeliveryService;
 import com.example.i_commerce.domain.order.service.dto.DeliveryCancelRequestEvent;
 import com.example.i_commerce.global.exception.AppException;
 import java.util.List;
+>>>>>>>> 13c6152 (feat: 결제 취소 요청시 배송 상태 변경 이벤트 처리):src/main/java/com/example/i_commerce/domain/order/event/listener/DeliveryListener.java
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -17,13 +21,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+<<<<<<<< HEAD:src/main/java/com/example/i_commerce/domain/order/event/listener/PaymentLogListener.java
+public class PaymentLogListener {
+========
 public class DeliveryListener {
+>>>>>>>> 13c6152 (feat: 결제 취소 요청시 배송 상태 변경 이벤트 처리):src/main/java/com/example/i_commerce/domain/order/event/listener/DeliveryListener.java
 
-    private final DeliveryService deliveryService;
+    private final PaymentHistoryService paymentHistoryService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+<<<<<<<< HEAD:src/main/java/com/example/i_commerce/domain/order/event/listener/PaymentLogListener.java
+    public void createPaymentHistoryRequest(PaymentCompletedEvent event) {
+        paymentHistoryService.createPaymentHistory(event);
+========
     public void handlePaymentCompleted(PaymentCompletedEvent event) {
         deliveryService.createDelivery(event);
+>>>>>>>> 13c6152 (feat: 결제 취소 요청시 배송 상태 변경 이벤트 처리):src/main/java/com/example/i_commerce/domain/order/event/listener/DeliveryListener.java
     }
 
     @EventListener
