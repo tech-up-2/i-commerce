29edc21dc5c77aa9e2e48cde75f6b106439d6232
muncool39 <muncool39@gmail.com>
2026-05-12 16:45:40 +0900

    feat: 재고 수량 초과한 경우의 오류 처리 추가 (#54)

--- src/main/java/com/example/i_commerce/domain/cart/entity/CartItem.java
+++ src/main/java/com/example/i_commerce/domain/cart/entity/CartItem.java
@@ -1,6 +1,8 @@
 package com.example.i_commerce.domain.cart.entity;
 
+import com.example.i_commerce.domain.cart.exception.CartErrorCode;
 import com.example.i_commerce.global.common.entity.BaseEntity;
+import com.example.i_commerce.global.exception.AppException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -48,5 +50,13 @@ public class CartItem extends BaseEntity {
 
     @Builder.Default
     private Boolean isChecked = true;
+
+    public void increaseQuantity(int amount, int stockQuantity) {
+        int newQuantity = this.quantity + amount;
+        if (newQuantity > stockQuantity) {
+            throw new AppException(CartErrorCode.EXCEED_STOCK_QUANTITY);
+        }
+        this.quantity = newQuantity;
+    }
 
 }
