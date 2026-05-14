b44669b0b7cec65b9658bb4decb8f1e6c7032231
muncool39 <muncool39@gmail.com>
2026-05-15 03:31:52 +0900

    refactor: 옵션 테이블 수정 (#73)
    - inputType 타입을 Enum으로 변경
    - 필요하지 않은 필드 삭제

--- src/main/java/com/example/i_commerce/domain/product/entity/Option.java
+++ src/main/java/com/example/i_commerce/domain/product/entity/Option.java
@@ -4,11 +4,14 @@ import com.example.i_commerce.global.common.entity.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
@@ -35,11 +38,9 @@ public class Option extends BaseEntity {
     @Column(length = 100, nullable = false)
     private String value;
 
-    @Column(length = 100, nullable = false)
-    private String displayName;
-
+    @Enumerated(EnumType.STRING)
     @Column(length = 100)
-    private String inputType;
+    private OptionInputType inputType;
 
     @Builder.Default
     @OneToMany(mappedBy = "option", cascade = CascadeType.ALL, orphanRemoval = true)
