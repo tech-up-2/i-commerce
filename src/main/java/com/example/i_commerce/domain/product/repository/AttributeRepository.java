220b595c24a7d610392af65271c5bebcfaef7cd9
muncool39 <muncool39@gmail.com>
2026-05-15 06:48:31 +0900

    feat: 속성 전체 조회 서비스 로직 (#74)

--- src/main/java/com/example/i_commerce/domain/product/repository/AttributeRepository.java
+++ src/main/java/com/example/i_commerce/domain/product/repository/AttributeRepository.java
@@ -1,7 +1,9 @@
 package com.example.i_commerce.domain.product.repository;
 
 import com.example.i_commerce.domain.product.entity.Attribute;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -10,4 +12,6 @@ public interface AttributeRepository extends JpaRepository<Attribute, Long> {
 
     boolean existsByKey(String key);
 
+    @Query("SELECT a FROM Attribute a ORDER BY a.key ASC, a.value ASC")
+    List<Attribute> findAllOrderedByKeyAndValue();
 }
