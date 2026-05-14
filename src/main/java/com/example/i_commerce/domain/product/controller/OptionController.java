5a2506e1120a68240d15a6a4997424d70bfb09ed
muncool39 <muncool39@gmail.com>
2026-05-15 04:44:36 +0900

    feat: 옵션 전체 조회 api (#73)

--- src/main/java/com/example/i_commerce/domain/product/controller/OptionController.java
+++ src/main/java/com/example/i_commerce/domain/product/controller/OptionController.java
@@ -3,9 +3,12 @@ package com.example.i_commerce.domain.product.controller;
 
 import com.example.i_commerce.domain.product.application.service.OptionService;
 import com.example.i_commerce.domain.product.controller.request.CreateOptionRequest;
+import com.example.i_commerce.domain.product.controller.response.OptionGroupResponse;
 import com.example.i_commerce.global.common.response.ApiResponse;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,12 @@ public class OptionController {
     ) {
         optionService.createOption(request);
         return ApiResponse.success();
+    }
+
+    @GetMapping
+    public ApiResponse<List<OptionGroupResponse>> getAllOptions() {
+        List<OptionGroupResponse> res = optionService.getAllOptionsGroupedByType();
+        return ApiResponse.success(res);
     }
 
 }
