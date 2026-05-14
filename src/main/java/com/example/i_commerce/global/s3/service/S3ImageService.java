f7322c884b1aaff339a68b4c16632c60c3d70ae4
jihoon <wlgnstls0413@naver.com>
2026-05-14 17:09:52 +0900

    chore

--- src/main/java/com/example/i_commerce/global/s3/service/S3ImageService.java
+++ src/main/java/com/example/i_commerce/global/s3/service/S3ImageService.java
@@ -47,6 +47,7 @@ public class S3ImageService {
             throw new RuntimeException("S3 파일 업로드 중 오류가 발생했습니다.", e);
         }
     }
+
     public void deleteImage(String fileUrl) {
         if (fileUrl == null || fileUrl.isEmpty()) {
             return;
