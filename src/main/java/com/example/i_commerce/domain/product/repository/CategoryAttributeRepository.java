4d4d5988950717ba3cd2c0b658cd885901cca1d4
muncool39 <muncool39@gmail.com>
2026-05-16 22:16:40 +0900

    feat: 이미 추가된 속성인지 확인하기 위한 쿼리 및 projection 추가 (#81)

--- src/main/java/com/example/i_commerce/domain/product/repository/CategoryAttributeRepository.java
+++ src/main/java/com/example/i_commerce/domain/product/repository/CategoryAttributeRepository.java
@@ -2,6 +2,7 @@ package com.example.i_commerce.domain.product.repository;
 
 
 import com.example.i_commerce.domain.product.entity.CategoryAttribute;
+import com.example.i_commerce.domain.product.repository.projection.CategoryAttributeKey;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,10 +12,24 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoryAttributeRepository extends JpaRepository<CategoryAttribute, Long> {
 
+    @Query("""
+    SELECT new com.example.i_commerce.domain.product.repository.projection.CategoryAttributeKey(
+        ca.category.id,
+        ca.attribute.id
+    )
+    FROM CategoryAttribute ca
+    WHERE ca.category.id IN :categoryIds
+    AND ca.attribute.id IN :attributeIds
+    """)
+    List<CategoryAttributeKey> findExistingKeys(
+        @Param("categoryIds") List<Long> categoryIds,
+        @Param("attributeIds") List<Long> attributeIds
+    );
+
+
     @Query("SELECT ca FROM CategoryAttribute ca "
         + "JOIN FETCH ca.attribute "
         + "WHERE ca.category.id = :categoryId ")
     List<CategoryAttribute> findByCategoryIdWithAttribute(@Param("categoryId") Long categoryId);
-
 
 }
