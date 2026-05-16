b4a91f3a40f94bdab38f3c2d90509f569b914e7f
muncool39 <muncool39@gmail.com>
2026-05-16 23:46:54 +0900

    feat: 카테고리 제공 속성 조회 api (#81)

--- src/main/java/com/example/i_commerce/domain/product/controller/CategoryAttributeController.java
+++ src/main/java/com/example/i_commerce/domain/product/controller/CategoryAttributeController.java
@@ -4,9 +4,11 @@ package com.example.i_commerce.domain.product.controller;
 import com.example.i_commerce.domain.product.application.service.CategoryAttributeService;
 import com.example.i_commerce.domain.product.controller.request.AddCategoryAttributeRequest;
 import com.example.i_commerce.domain.product.controller.response.AddCategoryAttributeResponse;
+import com.example.i_commerce.domain.product.controller.response.CategoryAttributeResponse;
 import com.example.i_commerce.global.common.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryAttributeController {
 
     private final CategoryAttributeService categoryAttributeService;
-    
+
+    @GetMapping
+    public ApiResponse<CategoryAttributeResponse> getCategoryAttributes(
+        @PathVariable Long categoryId
+    ){
+        CategoryAttributeResponse res =
+            categoryAttributeService.getAttributesByCategory(categoryId);
+        return ApiResponse.success(res);
+    }
+
 
     @PostMapping
     public ApiResponse<AddCategoryAttributeResponse> addCategoryAttribute(
