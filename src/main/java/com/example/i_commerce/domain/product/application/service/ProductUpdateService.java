6f1339a3535f151667f1277402f07644b0af952d
muncool39 <muncool39@gmail.com>
2026-05-16 14:16:45 +0900

    feat: 상품 상태 변경 서비스 로직 (#77)
    - 쿼리 추가
    - 응답 객체 추가

---
+++ src/main/java/com/example/i_commerce/domain/product/application/service/ProductUpdateService.java
@@ -0,0 +1,34 @@
+package com.example.i_commerce.domain.product.application.service;
+
+
+import com.example.i_commerce.domain.product.controller.request.UpdateProductStatusRequest;
+import com.example.i_commerce.domain.product.controller.response.UpdateProductStatusResponse;
+import com.example.i_commerce.domain.product.entity.Product;
+import com.example.i_commerce.domain.product.exception.ProductErrorCode;
+import com.example.i_commerce.domain.product.repository.ProductRepository;
+import com.example.i_commerce.global.exception.AppException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ProductUpdateService {
+
+    private final ProductRepository productRepository;
+
+    public UpdateProductStatusResponse changeProductStatus(
+        Long productId,
+        Long userId,
+        UpdateProductStatusRequest request
+    ) {
+        Product product = ( request.status().requiresItemCascade()
+            ? productRepository.findByIdWithItemsAndStock(productId)
+            : productRepository.findById(productId)
+        ).orElseThrow(() -> new AppException(ProductErrorCode.PRODUCT_NOT_FOUND));
+
+        product.changeStatus(request.status());
+        return UpdateProductStatusResponse.from(product);
+    }
+}
