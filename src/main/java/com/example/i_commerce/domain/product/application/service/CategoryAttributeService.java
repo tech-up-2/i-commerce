6bc170cdfc2766d1e35a025310b36a2cbb10265e
muncool39 <muncool39@gmail.com>
2026-05-16 23:45:57 +0900

    feat: 카테고리 제공 속성 조회 서비스 로직 (#81)

--- src/main/java/com/example/i_commerce/domain/product/application/service/CategoryAttributeService.java
+++ src/main/java/com/example/i_commerce/domain/product/application/service/CategoryAttributeService.java
@@ -1,9 +1,12 @@
 package com.example.i_commerce.domain.product.application.service;
 
 
+import com.example.i_commerce.domain.product.application.mapper.CategoryAttributeMapper;
 import com.example.i_commerce.domain.product.controller.request.AddCategoryAttributeRequest;
 import com.example.i_commerce.domain.product.controller.response.AddCategoryAttributeResponse;
 import com.example.i_commerce.domain.product.controller.response.AddCategoryAttributeResponse.AlreadyExistsAttribute;
+import com.example.i_commerce.domain.product.application.dto.CategoryAttributeGroupDto;
+import com.example.i_commerce.domain.product.controller.response.CategoryAttributeResponse;
 import com.example.i_commerce.domain.product.entity.Attribute;
 import com.example.i_commerce.domain.product.entity.Category;
 import com.example.i_commerce.domain.product.entity.CategoryAttribute;
@@ -12,6 +15,7 @@ import com.example.i_commerce.domain.product.repository.AttributeRepository;
 import com.example.i_commerce.domain.product.repository.CategoryAttributeRepository;
 import com.example.i_commerce.domain.product.repository.CategoryRepository;
 import com.example.i_commerce.domain.product.repository.projection.CategoryAttributeKey;
+import com.example.i_commerce.domain.product.repository.projection.CategoryAttributeProjection;
 import com.example.i_commerce.global.exception.AppException;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -30,6 +34,23 @@ public class CategoryAttributeService {
     private final CategoryRepository categoryRepository;
     private final AttributeRepository attributeRepository;
     private final CategoryAttributeRepository categoryAttributeRepository;
+    private final CategoryAttributeMapper categoryAttributeMapper;
+
+    @Transactional(readOnly = true)
+    public CategoryAttributeResponse getAttributesByCategory(Long categoryId) {
+
+        if(!categoryRepository.existsById(categoryId)) {
+            throw new AppException(ProductErrorCode.CATEGORY_NOT_FOUND);
+        }
+
+        List<CategoryAttributeProjection> projections =
+            categoryAttributeRepository.findWithAttributeByCategoryId(categoryId);
+
+        List<CategoryAttributeGroupDto> groupedCategoryAttributes =
+            categoryAttributeMapper.toGroupResponse(projections);
+
+        return CategoryAttributeResponse.of(categoryId, groupedCategoryAttributes);
+    }
 
     @Transactional
     public AddCategoryAttributeResponse addCategoryAttribute(
