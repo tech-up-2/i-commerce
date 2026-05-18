8f1af3c1c98a91f3429c81d55741ae463db52287
muncool39 <muncool39@gmail.com>
2026-05-18 12:25:23 +0900

    docs: 상품 도메인 스웨거 명세 추가 (#90)

--- src/main/java/com/example/i_commerce/global/swagger/SwaggerConfig.java
+++ src/main/java/com/example/i_commerce/global/swagger/SwaggerConfig.java
@@ -73,7 +73,10 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
             .group("Product")
             .pathsToMatch(
-                "/api/v1/seller/products"
+                "/api/v1/products/**",
+                "/api/v1/categories/**",
+                "/api/v1/options/**",
+                "/api/v1/attributes/**"
             )
             .addOpenApiCustomizer(securityCustomizer())
             .build();
