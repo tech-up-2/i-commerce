c3cc7527d732e23ccccda323414a0b4d8b5b3542
jihoon <wlgnstls0413@naver.com>
2026-06-04 16:14:36 +0900

    refactor: 시나리오 테스트 작성을 위한수정

--- src/main/java/com/example/i_commerce/domain/order/service/dto/CreateOrderResponse.java
+++ src/main/java/com/example/i_commerce/domain/order/service/dto/CreateOrderResponse.java
@@ -8,12 +8,14 @@ import lombok.Builder;
 public record CreateOrderResponse(
         Long orderId,
         Integer amount,
+        String tossOrderId,
         String orderName
 ) {
-    public static CreateOrderResponse of(Order order, String firstProductName) {
+    public static CreateOrderResponse of(Order order, String tossOrderId, String firstProductName) {
         return CreateOrderResponse.builder()
                 .orderId(order.getId())
                 .amount(order.getTotalPayAmount())
+                .tossOrderId(tossOrderId)
                 .orderName(firstProductName + "항목 외 " + (order.getOrderProducts().size() - 1) + "건")
                 .build();
     }
