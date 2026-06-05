ec87fd9a9a92992dc32bb0529ebeced4ad575943
muncool39 <muncool39@gmail.com>
2026-06-05 11:03:29 +0900

    test: unit test - product search service (#145)

--- src/main/java/com/example/i_commerce/domain/product/presentation/request/SearchProductRequest.java
+++ src/main/java/com/example/i_commerce/domain/product/presentation/request/SearchProductRequest.java
@@ -4,7 +4,10 @@ import com.example.i_commerce.domain.product.repository.enums.ProductSortType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import java.util.List;
+import lombok.Builder;
 
+
+@Builder
 public record SearchProductRequest(
     @Size(min = 2, message = "검색어는 2글자 이상이어야 합니다.")
     String keyword,
