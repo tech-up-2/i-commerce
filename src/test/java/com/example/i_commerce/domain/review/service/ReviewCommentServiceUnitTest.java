b24706f602249e2d11b6a97d057ba260d8039ba1
Yeojin <qhshqhsh3124287@gmail.com>
2026-06-08 10:09:46 +0900

    fix: @Builder.Default(#146)

---
+++ src/test/java/com/example/i_commerce/domain/review/service/ReviewCommentServiceUnitTest.java
@@ -0,0 +1,25 @@
+package com.example.i_commerce.domain.review.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewCommentServiceUnitTest {
+
+    @InjectMocks
+    private ReviewCommentService reviewCommentService;
+
+    @Test
+    @DisplayName("사용자는 리뷰에 답글을 작성할 수 있다.")
+    void createComment() {
+        //given
+
+
+        //when
+
+        //then
+    }
+}
