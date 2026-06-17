a6fde84bfb244904289f5bac556ef447d56fc3c5
jihoon <wlgnstls0413@naver.com>
2026-06-17 11:12:55 +0900

    feat: 외부 API설정

--- src/main/java/com/example/i_commerce/domain/order/support/MockCourierComponent.java
+++ src/main/java/com/example/i_commerce/domain/order/support/MockCourierComponent.java
@@ -28,33 +28,39 @@ public class MockCourierComponent {
     // 부하 테스트 시 결제/배송시작 API 스레드를 방해하지 않기 위한 독립 스레드 풀
     @Bean(name = "courierThreadPool")
     public Executor courierThreadPool() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(30);
-        executor.setMaxPoolSize(30);
-        executor.setQueueCapacity(200);
-        executor.setThreadNamePrefix("mock-courier-thread-");
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        executor.initialize();
-        return executor;
+        return Executors.newVirtualThreadPerTaskExecutor();
     }
+
+//    @Bean(name = "courierThreadPool")
+//    public Executor courierThreadPool() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(30);
+//        executor.setMaxPoolSize(30);
+//        executor.setQueueCapacity(200);
+//        executor.setThreadNamePrefix("mock-courier-thread-");
+//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+//        executor.initialize();
+//        return executor;
+//    }
 
     @Async("courierThreadPool")
     @TransactionalEventListener(
             phase = TransactionPhase.AFTER_COMMIT,
             condition = "#event.currentStatus.name() == 'SHIPPING'"
     )
+
     public void onDeliveryStatusChanged(DeliveryStatusChangedEvent event) {
         Long orderId = event.orderId();
         Long deliveryId = event.deliveryId();
-            try {
-                long randomSleep = (long) (Math.random() * 500) + 500;
-                Thread.sleep(randomSleep);
+        try {
+            long randomSleep = (long) (Math.random() * 500) + 500;
+            Thread.sleep(randomSleep);
 
-                log.info("====== [가상 택배사] 배송 ID {} 배송 완료 처리 진행 ======", deliveryId);
-                 sellerDeliveryService.completeDelivery(orderId, deliveryId);
+            log.info("====== [가상 택배사] 배송 ID {} 배송 완료 처리 진행 ======", deliveryId);
+            sellerDeliveryService.completeDelivery(orderId, deliveryId);
 
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
